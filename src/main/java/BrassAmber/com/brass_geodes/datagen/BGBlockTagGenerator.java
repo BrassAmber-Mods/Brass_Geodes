96f2a9cde7f5c3195883627244eaf184a6ba0b74
JMilamber <socky79.turbulance@gmail.com>
2025-10-05 00:30:46 -0400

    Auto generate additions to minecraft block tags. Trunks require axes

--- src/main/java/BrassAmber/com/brass_geodes/datagen/BGBlockTagGenerator.java
+++ src/main/java/BrassAmber/com/brass_geodes/datagen/BGBlockTagGenerator.java
@@ -5,6 +5,7 @@ import BrassAmber.com.brass_geodes.BrassGeodes;
 import BrassAmber.com.brass_geodes.util.BGTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
@@ -39,6 +40,97 @@ public class BGBlockTagGenerator extends BlockTagsProvider  {
                 .add(BGBlocks.RUBY_TINTED_GLASS.get())
                 .add(BGBlocks.EMERALD_TINTED_GLASS.get())
                 .add(BGBlocks.DIAMOND_TINTED_GLASS.get());
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(BGBlocks.BUDDING_TOPAZ.get())
+                .add(BGBlocks.BUDDING_SAPPHIRE.get())
+                .add(BGBlocks.BUDDING_RUBY.get())
+                .add(BGBlocks.BUDDING_EMERALD.get())
+                .add(BGBlocks.BUDDING_DIAMOND.get())
+                .add(BGBlocks.RAW_TOPAZ_BLOCK.get())
+                .add(BGBlocks.RAW_SAPPHIRE_BLOCK.get())
+                .add(BGBlocks.RAW_RUBY_BLOCK.get())
+                .add(BGBlocks.RAW_EMERALD_BLOCK.get())
+                .add(BGBlocks.RAW_DIAMOND_BLOCK.get())
+                .add(BGBlocks.AMETHYST_GEM_BLOCK.get())
+                .add(BGBlocks.TOPAZ_GEM_BLOCK.get())
+                .add(BGBlocks.SAPPHIRE_GEM_BLOCK.get())
+                .add(BGBlocks.RUBY_GEM_BLOCK.get())
+                .add(BGBlocks.TOPAZ_CLUSTER.get())
+                .add(BGBlocks.SAPPHIRE_CLUSTER.get())
+                .add(BGBlocks.RUBY_CLUSTER.get())
+                .add(BGBlocks.EMERALD_CLUSTER.get())
+                .add(BGBlocks.DIAMOND_CLUSTER.get())
+                .add(BGBlocks.LARGE_TOPAZ_BUD.get())
+                .add(BGBlocks.LARGE_SAPPHIRE_BUD.get())
+                .add(BGBlocks.LARGE_RUBY_BUD.get())
+                .add(BGBlocks.LARGE_EMERALD_BUD.get())
+                .add(BGBlocks.LARGE_DIAMOND_BUD.get())
+                .add(BGBlocks.MEDIUM_TOPAZ_BUD.get())
+                .add(BGBlocks.MEDIUM_SAPPHIRE_BUD.get())
+                .add(BGBlocks.MEDIUM_RUBY_BUD.get())
+                .add(BGBlocks.MEDIUM_EMERALD_BUD.get())
+                .add(BGBlocks.MEDIUM_DIAMOND_BUD.get())
+                .add(BGBlocks.SMALL_TOPAZ_BUD.get())
+                .add(BGBlocks.SMALL_SAPPHIRE_BUD.get())
+                .add(BGBlocks.SMALL_RUBY_BUD.get())
+                .add(BGBlocks.SMALL_EMERALD_BUD.get())
+                .add(BGBlocks.SMALL_DIAMOND_BUD.get());
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(BGBlocks.BUDDING_TOPAZ.get())
+                .add(BGBlocks.BUDDING_SAPPHIRE.get())
+                .add(BGBlocks.BUDDING_RUBY.get())
+                .add(BGBlocks.BUDDING_EMERALD.get())
+                .add(BGBlocks.BUDDING_DIAMOND.get())
+                .add(BGBlocks.RAW_TOPAZ_BLOCK.get())
+                .add(BGBlocks.RAW_SAPPHIRE_BLOCK.get())
+                .add(BGBlocks.RAW_RUBY_BLOCK.get())
+                .add(BGBlocks.RAW_EMERALD_BLOCK.get())
+                .add(BGBlocks.RAW_DIAMOND_BLOCK.get())
+                .add(BGBlocks.AMETHYST_GEM_BLOCK.get())
+                .add(BGBlocks.TOPAZ_GEM_BLOCK.get())
+                .add(BGBlocks.SAPPHIRE_GEM_BLOCK.get())
+                .add(BGBlocks.RUBY_GEM_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_STONE_TOOL)
+                .add(BGBlocks.TOPAZ_CLUSTER.get())
+                .add(BGBlocks.SAPPHIRE_CLUSTER.get())
+                .add(BGBlocks.RUBY_CLUSTER.get())
+                .add(BGBlocks.EMERALD_CLUSTER.get())
+                .add(BGBlocks.DIAMOND_CLUSTER.get())
+                .add(BGBlocks.LARGE_TOPAZ_BUD.get())
+                .add(BGBlocks.LARGE_SAPPHIRE_BUD.get())
+                .add(BGBlocks.LARGE_RUBY_BUD.get())
+                .add(BGBlocks.LARGE_EMERALD_BUD.get())
+                .add(BGBlocks.LARGE_DIAMOND_BUD.get())
+                .add(BGBlocks.MEDIUM_TOPAZ_BUD.get())
+                .add(BGBlocks.MEDIUM_SAPPHIRE_BUD.get())
+                .add(BGBlocks.MEDIUM_RUBY_BUD.get())
+                .add(BGBlocks.MEDIUM_EMERALD_BUD.get())
+                .add(BGBlocks.MEDIUM_DIAMOND_BUD.get())
+                .add(BGBlocks.SMALL_TOPAZ_BUD.get())
+                .add(BGBlocks.SMALL_SAPPHIRE_BUD.get())
+                .add(BGBlocks.SMALL_RUBY_BUD.get())
+                .add(BGBlocks.SMALL_EMERALD_BUD.get())
+                .add(BGBlocks.SMALL_DIAMOND_BUD.get())
+                .add(BGBlocks.GEMCORN_TRUNK.get())
+                .add(BGBlocks.AMETHYST_TRUNK.get())
+                .add(BGBlocks.TOPAZ_TRUNK.get())
+                .add(BGBlocks.SAPPHIRE_TRUNK.get())
+                .add(BGBlocks.RUBY_TRUNK.get())
+                .add(BGBlocks.EMERALD_TRUNK.get())
+                .add(BGBlocks.DIAMOND_TRUNK.get());
+
+
+        this.tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(BGBlocks.GEMCORN_TRUNK.get())
+                .add(BGBlocks.AMETHYST_TRUNK.get())
+                .add(BGBlocks.TOPAZ_TRUNK.get())
+                .add(BGBlocks.SAPPHIRE_TRUNK.get())
+                .add(BGBlocks.RUBY_TRUNK.get())
+                .add(BGBlocks.EMERALD_TRUNK.get())
+                .add(BGBlocks.DIAMOND_TRUNK.get());
     }
 
     @Override
